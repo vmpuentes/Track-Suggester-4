701843f071d8c6d4216023ce26c4afbc1800f165
Victor Puentes <vmpuentes@gmail.com>
2017-09-20 16:46:21 -0700

    add syntax changes to scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   $("form#trackSurvey").submit(function(event) {
-    var userSelect = parseInt($("trackSurvey").val());
+    var userSelect = parseInt($("#trackSurvey").val());
     var resultArr = [];
   event.preventDefault();
-  if ($("q1").val() === 1 && $("q2").val() === 1) {
+  if ($('#q1').val() === 1 && $('#q2').val() === 1) {
     $("#javaCsharp").show();
-  } else if ($("q4").val() === 1 || ($("q3").val() === 2 || ($("q5").val() === 2)
-    $("#ruby").show()
-  } else if ($("q4").val(); === 2)
-    $("python").show()
-  } else if ($("q4").val(); === 3)
-    $("php").show()
+  } else if ($('#q4').val() === 1 || ($('#q3').val() === 2 || ($('#q5').val() === 2)
+    $("#ruby").show();;
+  } else if ($('#q4').val(); === 2)
+    $('#python').show();
+  } else if ($('#q4').val(); === 3)
+    $('#php').show();
   } else  {
-    $("javascript").show()
-  }
+    $('#javascript').show();
+    }
   });
 });
