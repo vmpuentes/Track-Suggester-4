8294070cd81e5da396fc1f0c8565ff9e66d1fdae
Victor Puentes <vmpuentes@gmail.com>
2017-09-19 21:21:14 -0700

    add changes to if statement in scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $("form#trackSurvey").submit(function(event) {
-    var userInput = parseInt($("trackSurvey").val());
-    var
+    var userSelect = parseInt($("trackSurvey").val());
+    var=
   event.preventDefault();
-}
+  if ($("q1").val(); === 1 && $("q2").val(); === 1) {
+    $("#javaCsharp").show();
+  } else if ($("q4").val(); === 1 || ($("q3").val(); === 2 || ($("q5").val(); === 2) {
+    $("#ruby").show();
+  } else if ($("q4").val(); === 2) {
+    $("python").show();
+  } else if ($("q4").val(); === 3) {
+    $("php").show();
+  } else  {
+    $("javascript").show();
+  }
+  });
+});
