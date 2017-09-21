e7815ec407bdd5eda2cb392ede0ba9bae464c0bf
Guest User <Guest@epicodus-7.local>
2017-09-21 13:10:42 -0700

    add display none for track id's in css file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
-  $('#trackSurvey').submit(function(event) {
+  $('form#trackSurvey').submit(function(event) {
   //  var userSelect = $("#trackSurvey").val();
   //  console.log("hello");s
 //    var resultArr = [];
-  var suggestedLanguage;
+  var suggestedLanguage = "";
   var questionOne = $('#q1').val().parseInt();
   var questionTwo = $('#q2').val().parseInt();
   var questionThree = $('#q3').val().parseInt();
   var questionFour = $('#q4').val().parseInt();
   var questionFive = $('#q5').val().parseInt();
-
+debugger;
   event.preventDefault();
   if (questionOne === 1 && questionTwo === 1) {
-    return $('#javaCsharp').show();
-  } else if (questionOne === 2 &&)
-  } else if (questionFour === 1 || (questionThree === 2 || (questionFive === 2)
-    return $('#ruby').show());
-  } else if (questionFour === 2)
-    return $('#python').show();
-  } else if (questionFour === 3)
-    return $('#php').show();
+    suggestedLanguage = $('#javaCsharp').show();
+  } else if (questionTwo === 2 && questionThree === 1) {
+    suggestedLanguage = $('p#javascript').show()
+  } else if (questionFour === 1 || questionThree === 2 || questionFive === 2) {
+    suggestedLanguage = $('p#ruby').show();
+  } else if (questionFour === 2) {
+    suggestedLanguage = $('p#python').show();
+  } else if (questionFour === 3) {
+    suggestedLanguage = $('p#php').show();
   } else  {
-    return $('#javascript').show();
+    suggestedLanguage = $('p#javascript').show();
+    console.log("hello");
     }
   });
 });
