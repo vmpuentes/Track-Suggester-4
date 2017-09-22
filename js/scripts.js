a7e5a96e60853de44d9b17b04a6637bfc70aac23
Victor Puentes <vmpuentes@gmail.com>
2017-09-22 09:00:58 -0700

    add final edits to html for spacing and indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 $(document).ready(function() {
   $('form#trackSurvey').submit(function(event) {
-  //  var userSelect = $("#trackSurvey").val();
-  //  console.log("hello");s
-//    var resultArr = [];
-
-  var questionOne = parseInt($('#q1').val());
-  var questionTwo = parseInt($('#q2').val());
-  var questionThree = parseInt($('#q3').val());
-  var questionFour = parseInt($('#q4').val());
-  var questionFive = parseInt($('#q5').val());
-  var suggestedLanguage = "";
-  //event.preventDefault();
+    var questionOne = parseInt($('#q1').val());
+    var questionTwo = parseInt($('#q2').val());
+    var questionThree = parseInt($('#q3').val());
+    var questionFour = parseInt($('#q4').val());
+    var questionFive = parseInt($('#q5').val());
   if (questionOne === 1 && questionTwo === 1) {
-    suggestedLanguage = $('#javaCsharp').show();
+    $('#javaCsharp').show();
+    $('#generalResponse').show();
   } else if (questionTwo === 2 && questionThree === 1) {
-    suggestedLanguage = $('#javascript').show()
-  } else if (questionFour === 1 || questionThree === 2 || questionFive === 2) {
-    suggestedLanguage = $('#ruby').show();
+    $('#javascript').show()
+    $('#css').show();
+    $('#generalResponse').show();
+  } else if (questionFour === 1 && questionThree === 2 || questionFive === 2) {
+    $('#ruby').show();
+    $('#css').show();
+    $('#generalResponse').show();
   } else if (questionFour === 2) {
-    suggestedLanguage = $('#python').show();
+    $('#python').show();
+    $('#css').show();
+    $('#generalResponse').show();
   } else if (questionFour === 3) {
-    suggestedLanguage = $('#php').show();
+    $('#php').show();
+    $('#css').show();
+    $('#generalResponse').show();
   } else  {
-    suggestedLanguage = $('#javascript').show();
-    console.log("hello");
+    $('#javascript').show();
+    $('#python').show();
+    $('#css').show();
+    $('#generalResponse').show();
     }
     event.preventDefault();
   });
